8d21aa179c21364bb9d853a291ec7cda344048b2
Martha Gonzalez and Patrick Napper <patrick@>
2017-06-19 20:22:22 -0700

    implemented additional behaviors.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,11 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
-      if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+      if (year % 4 === 0) && (year % 100 !== 0)) {
+        return true;
+      } else {
+        return false;
+      }
 };
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
