adc45ad2656d8b679e7506fe7291116f772f6352
Martha Gonzalez and Patrick Napper <martha@>
2017-06-19 20:18:00 -0700

    create initial project leap-year using behavior driven developement.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var leapYear = function(year) {
+      if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
